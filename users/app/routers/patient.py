91aac16fd634573d451968cfce2b03d3e1d01459
Alejandro Encarnacion <aencarnacion@truora.com>
2021-09-22 23:33:29 -0400

    Refactor patients router to handle error responses

--- users/app/routers/patient.py
+++ users/app/routers/patient.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
+import traceback
+from typing import List
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
 
 import storage
 from schemas.patient import PatientIn, Patient
@@ -12,28 +15,57 @@ router = APIRouter(
 create_tables()
 
 
-@router.post("/patients/", response_model=Patient)
+@router.post("/patients", response_model=Patient, status_code=status.HTTP_201_CREATED)
 def create_user(patient: PatientIn, db: Session = Depends(get_db)):
     try:
+        existing_patient = storage.get_patient_by_email(db, patient.user.email)
+        if existing_patient:
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    "message": f'User with email {patient.user.email} already exists'}
+            )
+
         return storage.create_patient(db, patient)
     except Exception:
-        raise HTTPException(
-            status_code=500, detail="Internal server error, try again later")
+        print(traceback.format_exc())
+
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"message": "Internal server error, try again later"}
+        )
 
 
-@router.get("/patients/{patient_id}", response_model=Patient)
+@router.get("/patients", response_model=List[Patient], status_code=status.HTTP_200_OK)
+def get_patients(db: Session = Depends(get_db)):
+    return storage.get_patients(db)
+
+
+@router.get("/patients/{patient_id}", response_model=Patient, status_code=status.HTTP_200_OK)
 def get_patient(patient_id: int, db: Session = Depends(get_db)):
     db_patient = storage.get_patient(db, patient_id)
     if db_patient is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        return JSONResponse(
+            status_code=404,
+            content={"message": "User not found"}
+        )
 
     return db_patient
 
 
-@router.delete("/patients/{patient_id}", response_model=Patient)
+@router.delete("/patients/{patient_id}", response_model=Patient, status_code=status.HTTP_200_OK)
 def delete_patient(patient_id: int, db: Session = Depends(get_db)):
-    db_patient = storage.delete_patient(db, patient_id)
-    if db_patient is None:
-        raise HTTPException(status_code=404, detail="User not found")
+    try:
+        db_patient = storage.delete_patient(db, patient_id)
+        if db_patient is None:
+            return JSONResponse(
+                status_code=404,
+                content={"message": "User not found"}
+            )
 
-    return db_patient
+        return db_patient
+    except Exception:
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"message": "Internal server error, try again later"}
+        )
