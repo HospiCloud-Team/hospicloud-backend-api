021c3676ec8ea46489842ddd84ead636fc71b896
frankchavez <robertofrank2000@hotmail.com>
2021-09-26 12:54:06 -0400

    Added main operations and dependencies

--- checkups/app/main.py
+++ checkups/app/main.py
@@ -1,18 +1,38 @@
-from common import database
-from fastapi import FastAPI, status
+from common import models, database
+from common.models import create_tables
+from dependencies import get_db
+import storage
+from typing import List
+from schemas.checkups import Checkup, CheckupIn
+from fastapi import FastAPI, status, Depends
 from starlette.responses import JSONResponse
 
-app = FastAPI(
-    title="Checkups", description="Checkups service for HospiCloud app."
+app = FastAPI(title="Checkups", description="Checkups service for HospiCloud app.")
+
+create_tables()
+
+
+@app.get(
+    "/checkups/doctor/{doctor_id}", response_model=List[Checkup], tags=["checkups"]
 )
+async def read_checkups_by_doctor(
+    doctor_id: int, skip: int = 0, limit: int = 100, db_session=Depends(get_db)
+):
+    checkups = storage.get_checkups_by_doctor(db_session, doctor_id)
+    return checkups
 
-engine = database.start_engine()
-database.create_tables(engine)
 
-@app.get("/", tags=["checkups"])
-async def read_checkups():
-    pass
+@app.get(
+    "/checkups/doctor/{patient_id}", response_model=List[Checkup], tags=["checkups"]
+)
+async def read_checkups_by_doctor(
+    patient_id: int, skip: int = 0, limit: int = 100, db_session=Depends(get_db)
+):
+    checkups = storage.get_checkups_by_patient(db_session, patient_id)
+    return checkups
 
-@app.post("/", tags=["checkups"])
-async def add_checkup():
-    pass
+
+@app.post("/checkups", tags=["checkups"])
+async def add_checkup(checkup: CheckupIn, db_session=Depends(get_db)):
+    checkup_db = storage.create_checkup(db_session, checkup)
+    return checkup_db
