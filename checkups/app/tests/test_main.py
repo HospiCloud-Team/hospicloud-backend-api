435be8d0b43ea46b14ef4626f1d26ebe6d01b3aa
Frank Chavez <robertofrank2000@hotmail.com>
2021-09-29 17:20:46 -0400

    Added tests for checkups

---
+++ checkups/app/tests/test_main.py
@@ -0,0 +1,89 @@
+import datetime
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import pytest
+
+from main import app
+from common.database import start_engine
+from common.models import Base, User, Patient, Doctor, Template
+from dependencies import get_db
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture()
+def test_db():
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    doctor_user = User(
+        user_role="doctor",
+        document_type="national_id",
+        name="test",
+        last_name="test",
+        email="test@doctor",
+        document_number="12345654321",
+        date_of_birth=datetime.datetime.strptime("01-20-2000", "%m-%d-%Y"),
+    )
+    patient_user = User(
+        document_type="national_id",
+        name="test",
+        last_name="test",
+        email="test@patient",
+        document_number="12345654321",
+        date_of_birth=datetime.datetime.strptime("01-20-2000", "%m-%d-%Y"),
+    )
+    doctor = Doctor(user=doctor_user)
+    patient = Patient(user=patient_user, id_blood_type="a_plus")
+    template = Template(
+        numeric_fields=0,
+        alphanumeric_fields=1,
+        file_upload_fields=0,
+        headers="{'test':'str'}",
+    )
+    db.add(doctor_user)
+    db.add(patient_user)
+    db.add(doctor)
+    db.add(patient)
+    db.add(template)
+    db.commit()
+    db.refresh(doctor)
+    db.refresh(patient)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
+
+
+def test_add_checkup(test_db):
+    payload = {
+        "data": "{'test':'test'}",
+        "doctor_id": 1,
+        "patient_id": 1,
+        "template_id": 1,
+    }
+    response = client.post("/checkups/", json=payload)
+    data = response.json()
+    print(data)
+    assert response.status_code == 201
+    assert data["doctor"]["id"] == 1
+    assert data["patient"]["id"] == 1
+    assert data["data"] == "{'test':'test'}"
