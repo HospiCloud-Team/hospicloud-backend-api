92e9aba988e66f2de080d81a4c52db32b67a1530
aledeltoro <alejandroencarnacion1@gmail.com>
2021-10-08 00:09:01 -0400

    Create tests for Users API

---
+++ users/app/tests/test_db.py
@@ -0,0 +1,66 @@
+import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import pytest
+
+
+from common.database import start_engine
+from common.models import Base, Hospital, BaseUser, Patient, Specialty, Schedule
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
+                       "check_same_thread": False})
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def test_db():
+    Base.metadata.create_all(bind=engine)
+
+    db = TestingSessionLocal()
+
+    patient_user = BaseUser(
+        user_role="patient",
+        document_type="national_id",
+        name="test",
+        last_name="test",
+        email="test@gmail.com",
+        document_number="12345654321",
+        date_of_birth=datetime.datetime.strptime("01-20-2000", "%m-%d-%Y"),
+    )
+
+    patient = Patient(user=patient_user, blood_type="a_plus")
+
+    hospital = Hospital(name="Mock hospital")
+
+    specialties = [
+        Specialty(name="pediatrician"),
+        Specialty(name="general")
+    ]
+
+    schedule = Schedule(
+        start_day="monday",
+        end_day="friday",
+        all_day=True
+    )
+
+    db.add(patient_user)
+    db.add(patient)
+    db.add(hospital)
+    db.bulk_save_objects(specialties)
+    db.add(schedule)
+    db.commit()
+
+    yield
+    Base.metadata.drop_all(bind=engine)
---
+++ users/app/tests/test_main.py
@@ -0,0 +1,141 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+from starlette.status import HTTP_200_OK
+
+from main import app
+from dependencies import get_db
+from tests.test_db import override_get_db, test_db
+
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
+
+
+def test_create_patient(test_db):
+    payload = {
+        "user_role": "patient",
+        "document_type": "national_id",
+        "name": "Alejandro",
+        "last_name": "del Toro",
+        "email": "alejandro79@gmail.com",
+        "document_number": "11111111111",
+        "date_of_birth": "2000-06-27",
+        "patient": {
+            "blood_type": "a_plus"
+        }
+    }
+
+    response = client.post("/users", json=payload)
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+def test_create_doctor(test_db):
+    payload = {
+        "user_role": "doctor",
+        "document_type": "national_id",
+        "name": "Alejandro",
+        "last_name": "del Toro",
+        "email": "alejandro79@gmail.com",
+        "document_number": "11111111111",
+        "date_of_birth": "2000-06-27",
+        "doctor": {
+            "hospital_id": 1,
+            "schedule_id": 1,
+            "specialty_ids": [1, 2]
+        }
+    }
+
+    response = client.post("/users", json=payload)
+    
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+def test_create_admin(test_db):
+    payload = {
+        "user_role": "admin",
+        "document_type": "national_id",
+        "name": "Alejandro",
+        "last_name": "del Toro",
+        "email": "alejandro79@gmail.com",
+        "document_number": "11111111111",
+        "date_of_birth": "2000-06-27",
+        "admin": {
+            "hospital_id": 1
+        }
+    }
+
+    response = client.post("/users", json=payload)
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+def test_create_patient_email_already_exists(test_db):
+    payload = {
+        "user_role": "patient",
+        "document_type": "national_id",
+        "name": "Alejandro",
+        "last_name": "del Toro",
+        "email": "alejandro79@gmail.com",
+        "document_number": "11111111111",
+        "date_of_birth": "2000-06-27",
+        "patient": {
+            "blood_type": "a_plus"
+        }
+    }
+
+    _ = client.post("/users", json=payload)
+    response = client.post("/users", json=payload)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_get_patient_by_id(test_db):
+    response = client.get("/users/1")
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_get_patient_not_found(test_db):
+    response = client.get("/users/123")
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_get_users_list(test_db):
+    response = client.get("/users")
+    data = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert len(data) > 0
+
+
+def test_delete_user(test_db):
+    response = client.delete("/users/1")
+
+    assert response.status_code == status.HTTP_200_OK
+
+    response = client.get("/users/1")
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_update_user(test_db):
+    payload = {
+        "name": "new name",
+        "last_name": "new last name",
+        "document_number": "12345654399",
+        "patient": {
+            "medical_background": "This is an update!"
+        }
+    }
+
+    response = client.put("/users/1", json=payload)
+    data = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert data["name"] == "new name"
+    assert data["last_name"] == "new last name"
+    assert data["document_number"] == "12345654399"
+    assert data["patient"]["medical_background"] == "This is an update!"
+    assert data["updated_at"] is not None
