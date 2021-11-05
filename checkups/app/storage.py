28b3aab7b318b875cdfbc5375fe9c058c17209f6
frankchavez <robertofrank2000@hotmail.com>
2021-11-04 20:20:32 -0400

    Added firebase-cloudauth to project

--- checkups/app/storage.py
+++ checkups/app/storage.py
@@ -1,10 +1,19 @@
+import os
+from fastapi.param_functions import Depends
 from sqlalchemy.orm import Session
 from typing import List
+from fastapi_cloudauth.firebase import FirebaseClaims, FirebaseCurrentUser
 from common.schemas.checkup import CheckupIn
 from common.models import Checkup
 
+get_current_user = FirebaseCurrentUser(project_id=os.getenv("PROJECT_ID"))
 
-def get_checkup(db: Session, checkup_id: int) -> Checkup:
+
+def get_checkup(
+    db: Session,
+    checkup_id: int,
+    current_user: FirebaseClaims = Depends(get_current_user),
+) -> Checkup:
     return db.query(Checkup).filter(Checkup.id == checkup_id).first()
 
 
