10da9f5f1c24523147aa4759240dc2934023082d
aledeltoro <alejandroencarnacion1@gmail.com>
2021-11-11 18:43:18 -0400

    Set "timezone" arg to True for columns of "DateTime" datetype

--- common/utils.py
+++ common/utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 import pytz
 
 DR_TIMEZONE_NAME = "America/Santo_Domingo"
@@ -6,5 +6,5 @@ DR_TIMEZONE_NAME = "America/Santo_Domingo"
 DR_TIMEZONE = pytz.timezone(DR_TIMEZONE_NAME)
 
 
-def get_current_time() -> datetime:
-    return datetime.now(DR_TIMEZONE)
+def get_current_time() -> datetime.datetime:
+    return datetime.datetime.now(datetime.timezone.utc)
