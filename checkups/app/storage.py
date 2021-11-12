74d23f223e0ce4e55dc02d87739d164a07f62214
aledeltoro <alejandroencarnacion1@gmail.com>
2021-11-12 00:33:24 -0400

    Update "Checkups" to assign datetime with a timezone

--- checkups/app/storage.py
+++ checkups/app/storage.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from common.schemas.checkup import CheckupIn
 from common.models import Checkup
+from common.utils import get_current_time
 
 
 def get_checkup(db: Session, checkup_id: int) -> Checkup:
@@ -18,9 +19,12 @@ def get_checkups_by_doctor(db: Session, doctor_id: int) -> List[Checkup]:
 
 def create_checkup(db: Session, checkup: CheckupIn) -> Checkup:
     new_checkup = Checkup(**checkup.dict())
+    new_checkup.date = get_current_time()
 
     db.add(new_checkup)
+
     db.commit()
+    db.refresh(new_checkup)
 
     return new_checkup
 
