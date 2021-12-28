329d0aeeaf53fbb77ef316da90490ab3adb2e3c2
aledeltoro <alejandroencarnacion1@gmail.com>
2021-12-27 21:13:01 -0400

    Add GET request to return user by document number

--- users/app/main.py
+++ users/app/main.py
@@ -154,7 +154,7 @@ async def get_users(
             return storage.get_users(db, hospital_id=current_user.hospital_id)
     else:
         return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             content={"message": "User is not an admin."},
         )
 
@@ -174,7 +174,35 @@ async def get_user(
     db_user = storage.get_user(db, user_id)
     if db_user is None:
         return JSONResponse(
-            status_code=status.HTTP_404_NOT_FOUND, content={"message": "User not found"}
+            status_code=status.HTTP_404_NOT_FOUND, content={
+                "message": "User not found"}
+        )
+    elif current_user.user_role not in [UserRole.admin, UserRole.doctor]:
+        return JSONResponse(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content={"message": "User not admin or doctor."},
+        )
+
+    return db_user
+
+
+@app.get(
+    "/users/document-number/{document_number}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    response_model_exclude_none=True,
+    tags=["users"]
+)
+async def get_user_with_document_number(
+    document_number: str,
+    db: Session = Depends(get_db),
+    current_user: FirebaseUser = Depends(get_current_user),
+):
+    db_user = storage.get_user(db, document_number=document_number)
+    if db_user is None:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "User not found"}
         )
     elif current_user.user_role not in [UserRole.admin, UserRole.doctor]:
         return JSONResponse(
